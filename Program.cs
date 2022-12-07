f6ca80b7c3cad5100781f09fc0d846dea524520d
vani shree <hvanishree.20@gmail.com>
2022-12-07 10:03:24 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Dotnet5ConsoleAppCICD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
